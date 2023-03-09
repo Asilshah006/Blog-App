10b8e5b2a202c8f297c4e1e22b6178ec99f2d169
Syed Asil Ali <asilshah17@gmail.com>
2023-03-09 11:05:09 +0500

    Custom Hooks Implemented

---
+++ src/api/hooks/UseAxiosFetchAPI.js
@@ -0,0 +1,47 @@
+import { useEffect , useState } from "react";
+import axios from "axios";
+
+const useAxiosFetchAPI = (dataurl) =>{
+    const [data , setData] =useState([]);
+    const [fetchError , setFetchError] = useState(null);
+    const [isLoading , setIsLoading] = useState(true);
+
+    useEffect(()=>{
+        let isMounted = true;
+        const source = axios.CancelToken.source();
+
+        const FetchAPI = async (url) =>{
+            setIsLoading(true);
+            try{
+                const response = await axios.get(url ,{
+                    CancelToken : source.cancel()
+                });
+                if(isMounted){
+                    setData(response.data);
+                    setFetchError(null);
+                }
+            }catch(err){
+                if(isMounted){
+                    setFetchError(err.message);
+                    setData([]);
+                }
+            }finally{
+                {isMounted && setTimeout(()=> setIsLoading(false) , 2000)}
+            }   
+        }
+
+        FetchAPI(dataurl);
+
+        const cleanUp = () =>{
+            console.log("Clean up Something")
+            isMounted = false;
+            source.cancel();
+        }
+
+        return cleanUp;
+    },[dataurl])
+
+    return {data , fetchError , isLoading}
+}
+
+export default useAxiosFetchAPI;
